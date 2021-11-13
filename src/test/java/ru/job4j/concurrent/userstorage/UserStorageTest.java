e031a295b55489700b35e25c5ba57cd7b50c0b36
Daria V <voronovadaria@gmail.com>
2021-11-13 22:21:39 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #228210] more tests added

--- src/test/java/ru/job4j/concurrent/userstorage/UserStorageTest.java
+++ src/test/java/ru/job4j/concurrent/userstorage/UserStorageTest.java
@@ -38,4 +38,26 @@ public class UserStorageTest {
         assertEquals(8000, user1.getAmount());
         assertEquals(9600, user2.getAmount());
     }
+
+    @Test
+    public void transferAllMoney() {
+        User user1 = new User(1, 8000);
+        User user2 = new User(2, 9600);
+        storage.add(user1);
+        storage.add(user2);
+        storage.transfer(1, 2, 8000);
+        assertEquals(0, user1.getAmount());
+        assertEquals(17600, user2.getAmount());
+    }
+
+    @Test
+    public void transferTooMuchMoney() {
+        User user1 = new User(1, 8000);
+        User user2 = new User(2, 9600);
+        storage.add(user1);
+        storage.add(user2);
+        storage.transfer(1, 2, 8001);
+        assertEquals(8000, user1.getAmount());
+        assertEquals(9600, user2.getAmount());
+    }
 }
