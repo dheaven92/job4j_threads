5d34f25855b756b13e059941363cce75c0596391
Daria V <voronovadaria@gmail.com>
2021-11-13 17:58:21 +0300

    4. Скачивание файла с ограничением. [#144271 #227861] fixed

--- src/main/java/ru/job4j/concurrent/WgetLimited.java
+++ src/main/java/ru/job4j/concurrent/WgetLimited.java
@@ -4,10 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 public class WgetLimited implements Runnable {
 
-    private final static int PART_SIZE = 1024;
     private final String url;
     private final int speed;
 
@@ -19,23 +19,30 @@ public class WgetLimited implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream(getFileNameFromUrl(url))) {
-            byte[] dataBuffer = new byte[PART_SIZE];
+             FileOutputStream out = new FileOutputStream(Paths.get(new URL(url).getPath()).getFileName().toString())) {
+            byte[] dataBuffer = new byte[speed];
             int bytesRead;
-            long startTotalTime = System.currentTimeMillis();
-            long startPartTime = startTotalTime;
-            long size = 0;
-            while ((bytesRead = in.read(dataBuffer, 0, PART_SIZE)) != -1) {
+            long startTime = System.currentTimeMillis();
+            long lastLimitTime = System.currentTimeMillis();
+            long bytesTotalWritten = 0;
+            long bytesLimitWritten = 0;
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 out.write(dataBuffer, 0, bytesRead);
-                size += bytesRead;
-                long endPartTime = System.currentTimeMillis() - startPartTime;
-                startPartTime = System.currentTimeMillis();
-                if (endPartTime < speed) {
-                    Thread.sleep(speed - endPartTime);
+                bytesTotalWritten += bytesRead;
+                bytesLimitWritten += bytesRead;
+                if (bytesLimitWritten >= speed) {
+                    long deltaTime = System.currentTimeMillis() - lastLimitTime;
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
+                    }
+                    bytesLimitWritten = 0;
+                    lastLimitTime = System.currentTimeMillis();
                 }
             }
-            System.out.printf("Downloading %s b took %s ms with speed %s b/s",
-                    size, (System.currentTimeMillis() - startTotalTime), speed);
+            System.out.printf("Downloading %s KB took %s s with speed %s KB/s",
+                    convertBytesToKiloBytes(bytesTotalWritten),
+                    (System.currentTimeMillis() - startTime) / 1000,
+                    convertBytesToKiloBytes(speed));
         } catch (IOException | InterruptedException e) {
             if (e instanceof InterruptedException) {
                 Thread.currentThread().interrupt();
@@ -45,16 +52,18 @@ public class WgetLimited implements Runnable {
         }
     }
 
-    private String getFileNameFromUrl(String url) {
-        try {
-            String[] parts = url.split("/");
-            return "tmp_" + parts[parts.length - 1];
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid url format");
+    private static void validateArgs(int argsNum) {
+        if (argsNum != 2) {
+            throw new IllegalArgumentException("You must provide 2 arguments (file name and speed limit)");
         }
     }
 
+    private static long convertBytesToKiloBytes(long bytes) {
+        return Math.round((double) bytes / 1024);
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        validateArgs(args.length);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new WgetLimited(url, speed));
